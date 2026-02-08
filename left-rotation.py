91cf61aefaac17368722f53080d53e15ec223a64
Abayomi Eniola <eniolafaithful57@gmail.com>
2026-02-08 20:41:38 +0100

    Create left-rotation.py

---
+++ left-rotation.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+def rotateLeft(d, arr):
+    # Write your code here
+    if d < len(arr):
+        return arr[d:] + arr[0:d]
+    else:
+        return arr
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    d = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = rotateLeft(d, arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
