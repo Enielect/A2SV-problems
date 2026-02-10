f04fa4baed913065242f3b9f4478afabccadef86
Abayomi Eniola <eniolafaithful57@gmail.com>
2026-02-11 00:31:00 +0100

    Create reverse-string.py

---
+++ reverse-string.py
@@ -0,0 +1,20 @@
+# intial approach
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        n = len(s)
+        for i in range(n // 2):
+            s[i], s[n-i-1] = s[n-i-1], s[i]
+# cleaner and better
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l, r = 0, len(s) - 1
+        while l < r:
+            s[l], s[r] = s[r], s[l]
+            l +=1
+            r -=1
