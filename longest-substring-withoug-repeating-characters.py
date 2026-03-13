40051d6a6d64a73160dcaf3a22aba5e6b2fe4fa1
Abayomi Eniola <eniolafaithful57@gmail.com>
2026-03-13 23:09:29 +0100

    Create longest-substring-withoug-repeating-characters.py

---
+++ longest-substring-withoug-repeating-characters.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s:
+            return 0
+        l, r = 0, 0
+        seen, size = set(), 0
+        while r < len(s):
+            if s[r] in seen:
+                while s[l] != s[r]:
+                    seen.remove(s[l])
+                    l +=1
+                l +=1
+            else: seen.add(s[r])
+            size = max(size, r - l + 1)
+            r +=1
+        return size
