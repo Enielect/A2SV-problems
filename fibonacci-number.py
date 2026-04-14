4a31a7acf526efb1f3adc3c7b052686d6f8423f4
Abayomi Eniola <eniolafaithful57@gmail.com>
2026-04-14 15:43:15 +0100

    Create fibonacci-number.py

---
+++ fibonacci-number.py
@@ -0,0 +1,19 @@
+class Solution:
+    def fib(self, n: int) -> int:
+        # using dp
+        if n < 2:
+            return n
+            
+        dp = [0] * (n + 1)
+        dp[1] = 1
+
+        for i in range(2, n + 1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[-1]
+
+class Solution:
+    def fib(self, n: int) -> int:
+        # using recursion
+        if n < 2:
+            return n
+        return self.fib(n-1) + self.fib(n-2)
