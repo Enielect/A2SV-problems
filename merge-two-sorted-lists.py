2189786118602c70ad7ce1138d900500ed31aafd
Abayomi Eniola <eniolafaithful57@gmail.com>
2026-04-20 13:49:40 +0100

    Update merge-two-sorted-lists.py

--- merge-two-sorted-lists.py
+++ merge-two-sorted-lists.py
@@ -39,3 +39,32 @@ class Solution:
             cur = cur.next
         cur.next = list1 or list2
         return dummy.next
+
+# recursive solution
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode()
+        cur = dummy
+        
+        def sort(l1, l2, cur):
+            if not l1 or not l2:
+                cur.next = l1 or l2
+                return
+                
+            if l1.val <= l2.val:
+                val = l1.val
+                cur.next = ListNode(val)
+                sort(l1.next, l2, cur.next)
+            else:
+                val = l2.val
+                cur.next = ListNode(val)
+                sort(l1, l2.next, cur.next)
+
+        sort(list1, list2, cur)
+        return dummy.next
